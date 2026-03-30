24f261cec225729fd576cb7b33d9fdc034d859d0
thecodemaster12 <thecodemaster26@gmail.com>
2026-03-30 16:47:07 +0600

    added fake data

--- frontend/src/pages/Tasks.jsx
+++ frontend/src/pages/Tasks.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 const Tasks = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect (() => {
+    axios.get('http://localhost:3000/tasks')
+    .then((res) => {
+      setTasks(res.data)
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  },[])
   return (
-    <div>Tasks</div>
+    <main>
+      <div className="container mx-auto">
+        <div>Tasks: {tasks.length}</div>
+        {tasks.map((task) => (
+          <h2 key={task.id}>{task.text}</h2>
+        ))}
+      </div>
+    </main>
   )
 }
 
--- frontend/src/pages/Workspaces.jsx
+++ frontend/src/pages/Workspaces.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 const Workspaces = () => {
+  // const [loading, setLoading] = useState(true);
+  // const [error, setError] = useState(null);
+  const [workspace, setWorkspace] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/workspaces')
+            .then(res => {
+                setWorkspace(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }, []);
+
   return (
-    <div>Workspaces</div>
+      <main>
+          <div className="container mx-auto">
+            <h2>Workspaces: {workspace.length}</h2>
+          </div>
+      </main>
   )
 }
 
